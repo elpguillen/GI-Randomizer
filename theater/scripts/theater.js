5570909a831511191eedf41c0ae451c81cce436e
elpguillen <elpguillen@users.noreply.github.com>
2024-07-29 18:36:49 -0700

    Add createCharacterCard which will create a button that will represent the character card for a given character.

--- theater/scripts/theater.js
+++ theater/scripts/theater.js
@@ -16,5 +16,38 @@ function createCharacterGrid(characters) {
     // TODO: add character cards and event listener for each character card
 }
 
+/**
+ * Creates the Button to display the character.
+ * @param {string} character Character name
+ * @returns Button representing the given character
+ */
+function createCharacterCard(character) {
+    let characterButton = document.createElement("button");
+    let characterImageHolder = document.createElement("div"); 
+    let characterImage = document.createElement("img");  
+    let characterNameHolder = document.createElement("div"); 
+    let characterName = document.createTextNode(character);
+
+    characterButton.classList.add("character-card-button");
+    characterImageHolder.classList.add("character-image-holder");
+    characterNameHolder.classList.add("character-name-holder");
+
+    let currentCharacter = character;
+    let currentCharRarity = characters[currentCharacter]["rarity"];
+    changeCharBackground(currentCharRarity, characterImageHolder);
+
+    characterImage.src = "/icons/" + toLowerAndHyphenate(character) + ".png";
+    characterImage.setAttribute('draggable', false);
+    
+    characterImageHolder.appendChild(characterImage);
+
+    characterButton.appendChild(characterImageHolder);
+    characterButton.appendChild(characterNameHolder);
+
+    characterNameHolder.appendChild(characterName);
+
+    return characterButton;
+}
+
 
 createCharacterGrid("");
